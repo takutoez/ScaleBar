48a11d900f79deb527e361ea700d0e0a98ad5870
六車卓土 <takutoez@gmail.com>
2024-06-06 20:28:19 +0900

    Update ScaleText.js

--- src/ScaleText.js
+++ src/ScaleText.js
@@ -15,10 +15,10 @@ export default class ScaleText extends Component<Props> {
   render() {
     const svg = Skia.SVG.MakeFromString(
       `<svg xmlns="http://www.w3.org/2000/svg" width="0.6" height="${this.props.fontSize + 2}" viewBox="0 0 0.6 ${this.props.fontSize + 2}">
-          <text text-anchor="left" x="0" y="0" font-weight="500" stroke="#FFF" stroke-width="1.5" paint-order="stroke" font-family="ヒラギノ角ゴシック" font-size="${this.props.fontSize}">
+          <text text-anchor="left" x="0" y="${this.props.fontSize}" font-weight="500" stroke="#FFF" stroke-width="1.5" paint-order="stroke" font-size="${this.props.fontSize}">
             ${this.props.text}
           </text>
-          <text text-anchor="left" x="0" y="0" font-weight="500" fill="#000" font-family="ヒラギノ角ゴシック" font-size="${this.props.fontSize}">
+          <text text-anchor="left" x="0" y="${this.props.fontSize}" font-weight="500" fill="#000" font-size="${this.props.fontSize}">
             ${this.props.text}
           </text>
       </svg>`,
@@ -32,8 +32,8 @@ export default class ScaleText extends Component<Props> {
           left: this.props.padding_left + 3,
         }}
       >
-        <Canvas width={0.6} height={this.props.fontSize + 2}>
-          <ImageSVG svg={svg} width={0.6} height={this.props.fontSize + 2} />
+        <Canvas width={40} height={this.props.fontSize + 2}>
+          <ImageSVG svg={svg} width={40} height={this.props.fontSize + 2} />
         </Canvas>
       </View>
     );
